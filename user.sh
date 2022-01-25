5670c8eed40dad68bc3ce634fee9f13f66c710f9
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-01-25 11:57:09 -0800

    add user account

---
+++ user.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# User account (e.g. me)
+# For a regular user account, i'll add "well known directories"
+# and sudo privileges w/out password
+pacman -S xdg-user-dirs sudo
+useradd -m me
+passwd me
+echo 'me ALL=(ALL) NOPASSWD: ALL' > etc/sudoers.d/me
